7cd013db14abd745e5e0e23b868d302af4977f22
Trash Robot <lafelabs@gmail.com>
2022-08-19 16:52:46 -0400

    Update hyperlink.js

--- hyperlink/jscode/hyperlink.js
+++ hyperlink/jscode/hyperlink.js
@@ -1,6 +1,6 @@
 hyperlink = {
-    "url": "http://10.0.0.27/",
-    "src": "https://raw.githubusercontent.com/LafeLabs/crustweb/main/images/replicator.jpg",
+    "url": "http://10.0.0.138/",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/replicator.jpg",
     "alt": "cardboard sign linking to page",
     "list": [
         {
