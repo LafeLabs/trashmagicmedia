1a439eb4b8d5638bb924b63a25ec8f4ce080a9d4
Trash Robot <lafelabs@gmail.com>
2022-08-19 17:00:57 -0400

    prospectpark.xyz

--- hyperlink/prospectpark/jscode/hyperlink.js
+++ hyperlink/prospectpark/jscode/hyperlink.js
@@ -1,12 +1,8 @@
 hyperlink = {
-    "url": "http://10.0.0.138/",
-    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/replicator.jpg",
+    "url": "http://10.0.0.27/",
+    "src": "https://raw.githubusercontent.com/LafeLabs/crustweb/main/images/replicator.jpg",
     "alt": "cardboard sign linking to page",
     "list": [
-        {
-            "href": "fork.html",
-            "text": "fork"
-        },
         {
             "href": "../",
             "text": "up a level ../"
