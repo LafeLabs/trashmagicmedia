ed2ed7d325c41da9bd1f6837035738cd9e8edeb6
Trash Robot <lafelabs@gmail.com>
2022-08-19 17:15:05 -0400

    Update hyperlink.js

--- hyperlink/prospectpark/jscode/hyperlink.js
+++ hyperlink/prospectpark/jscode/hyperlink.js
@@ -1,23 +1,11 @@
 hyperlink = {
-    "url": "http://10.0.0.27/",
-    "src": "https://raw.githubusercontent.com/LafeLabs/crustweb/main/images/replicator.jpg",
+    "url": "https://wwww.prospectpark.xyz",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/prospectpark.jpg",
     "alt": "cardboard sign linking to page",
     "list": [
         {
-            "href": "../",
-            "text": "up a level ../"
-        },
-        {
-            "href": "https://www.trashrobot.org",
-            "text": "www.trashrobot.org"
-        },
-        {
-            "href": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/hyperlink/php/replicator.txt",
-            "text": "replicator"
-        },
-        {
-            "href": "https://www.southplatte.net",
-            "text": "southplatte.net"
+            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
         }
     ]
 };
