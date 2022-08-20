d870a8d862c08bfce93d096516f8c0240392fd41
Trash Robot <lafelabs@gmail.com>
2022-08-20 13:51:07 -0400

    maple lawn, trash magic

---
+++ hyperlink/maplelawn/southplatte/jscode/hyperlink.js
@@ -0,0 +1,23 @@
+hyperlink = {
+    "url": "http://www.southplatte.net",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/replicator.jpg",
+    "alt": "cardboard sign linking to page",
+    "list": [
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
+        },
+        {
+            "href": "fork.html",
+            "text": "FORK"
+        },
+        {
+            "href": "../",
+            "text": "up a level ../"
+        },
+        {
+            "href": "https://www.trashrobot.org",
+            "text": "www.trashrobot.org"
+        }
+    ]
+};
