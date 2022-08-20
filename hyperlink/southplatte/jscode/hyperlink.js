b4a57d60cd9aeb2620836db3bb2ecd659b10fa6b
Trash Robot <lafelabs@gmail.com>
2022-08-20 14:43:48 -0400

    south platte media

---
+++ hyperlink/southplatte/jscode/hyperlink.js
@@ -0,0 +1,19 @@
+hyperlink = {
+    "url": "http://www.southplatte.net",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/southplatte.jpg",
+    "alt": "cardboard sign linking to page",
+    "list": [
+        {
+            "href": "trashrobot/",
+            "text": "trashrobot/"
+        },
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
+        },
+        {
+            "href": "fork.html",
+            "text": "FORK"
+        }
+    ]
+};
---
+++ hyperlink/trashrobot/jscode/hyperlink.js
@@ -0,0 +1,47 @@
+hyperlink = {
+    "url": "http://www.trashrobot.org",
+    "src": "https://i.imgur.com/kHYleZH.jpg",
+    "alt": "flags",
+    "list": [
+        {
+            "href": "https://www.lulu.com/shop/trash-robot/geometron-magic/paperback/product-y6p96w.html",
+            "text": "buy geometron magic book from Lulu press"
+        },
+        {
+            "href": "https://www.trashrobot.org/user.php?scroll=scrolls/trashmagicbooks",
+            "text": "other trash magic books"
+        },
+        {
+            "href": "https://www.trashrobot.org/geometronmagic/",
+            "text": "geometron magic read online"
+        },
+        {
+            "href": "https://www.trashrobot.org/bookofgeometronfirst/",
+            "text": "first book of geometron read online"
+        },
+        {
+            "href": "https://www.trashrobot.org",
+            "text": "www.trashrobot.org"
+        },
+        {
+            "href": "../",
+            "text": "up a level"
+        },
+        {
+            "href": "https://www.tiktok.com/@trash_robot",
+            "text": "tiktok:@trash_robot"
+        },
+        {
+            "href": "https://github.com/LafeLabs/",
+            "text": "github:@lafelabs"
+        },
+        {
+            "href": "https://www.instagram.com/lafelabs/",
+            "text": "instagram:@lafelabs"
+        },
+        {
+            "href": "https://www.youtube.com/channel/UCLyeOlfnEBCnRTAH8rppEFw",
+            "text": "youtube:@trash_robot"
+        }
+    ]
+};
