eb7d7eb18a1a0b3fd0381ef9590cb3c9fc7e7da1
Trash Robot <lafelabs@gmail.com>
2022-08-21 14:51:52 -0400

    anarchistbookdistro

---
+++ anarchistbookdistro/hyperlink/jscode/hyperlink.js
@@ -0,0 +1,23 @@
+hyperlink = {
+    "url": "http://10.0.0.138/",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/replicator.jpg",
+    "alt": "cardboard sign linking to page",
+    "list": [
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
+        },
+        {
+            "href": "fork.html",
+            "text": "FORK"
+        },
+        {
+            "href": "../",
+            "text": "up a level ../"
+        },
+        {
+            "href": "https://www.trashrobot.org",
+            "text": "www.trashrobot.org"
+        }
+    ]
+};
---
+++ anarchistbookdistro/hyperlink/trashrobot/jscode/hyperlink.js
@@ -0,0 +1,35 @@
+hyperlink = {
+    "url": "https://www.trashrobot.org",
+    "src": "https://i.imgur.com/Uf14pkM.jpg",
+    "alt": "cardboard sign linking to page",
+    "list": [
+        {
+            "href": "https://www.trashrobot.org",
+            "text": "www.trashrobot.org"
+        },
+        {
+            "href": "http://10.0.0.7",
+            "text": "10.0.0.7"
+        },
+        {
+            "href": "http://10.0.0.27",
+            "text": "10.0.0.27"
+        },
+        {
+            "href": "https://www.southplatte.net",
+            "text": "www.southplatte.net"
+        },
+        {
+            "href": "https://www.tiktok.com/@trash_robot",
+            "text": "tiktok:@trash_robot"
+        },
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
+        },
+        {
+            "href": "../",
+            "text": "up a level ../"
+        }
+    ]
+};
