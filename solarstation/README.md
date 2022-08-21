46b552b608a04d686c93f2a3768aa0a91977411b
Trash Robot <lafelabs@gmail.com>
2022-08-21 12:27:39 -0400

    solarstation

---
+++ solarstation/README.md
@@ -0,0 +1,25 @@
+# [TRASH MAGIC MEDIA NETWORK](https://github.com/LafeLabs/trashmagicmedia)
+
+## [copy.php?from=
+
+# Solar Station
+
+Mutual Aid station.
+
+ - 12 volt lead acid battery
+ - volt meter
+ - standard training session in use of volt mater care of battery
+ - 20 watt solar panel
+ - battery charger/regulator
+
+### Links
+
+ - [images](images.html)
+ - [media](media.html)
+
+### Socials
+
+ - [tiktok:@trash_robot](https://www.tiktok.com/@trash_robot)
+ - [instagram:@lafelabs](https://www.instagram.com/lafelabs/)
+ - [twitter:@trashrobot0](https://twitter.com/trashrobot0)
+ - [youtube:@trash_robot](https://www.youtube.com/channel/UCLyeOlfnEBCnRTAH8rppEFw)
---
+++ solarstation/jscode/hyperlink.js
@@ -0,0 +1,43 @@
+hyperlink = {
+    "url": "http://10.0.0.27/",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/replicator.jpg",
+    "alt": "cardboard sign with trash magic logo",
+    "list": [
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
+        },
+        {
+            "href": "scroll.html",
+            "text": "TRASH MAGIC SCROLLS"
+        },
+        {
+            "href": "hyperlink/",
+            "text": "HYPERLINK PAGE BUILDER"
+        },
+        {
+            "href": "web/",
+            "text": "WEB"
+        },
+        {
+            "href": "images.html",
+            "text": "IMAGES"
+        },
+        {
+            "href": "media.html",
+            "text": "MEDIA"
+        },
+        {
+            "href": "fork.html",
+            "text": "FORK"
+        },
+        {
+            "href": "https://www.trashrobot.org",
+            "text": "www.trashrobot.org"
+        },
+        {
+            "href": "https://www.southplatte.net",
+            "text": "southplatte.net"
+        }
+    ]
+};
