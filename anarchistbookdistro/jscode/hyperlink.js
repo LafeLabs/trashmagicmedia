bab2293f6daca5dcf7ed33280d95cbd31bc1ece4
Trash Robot <lafelabs@gmail.com>
2022-08-21 14:57:00 -0400

    replicator.svg

--- anarchistbookdistro/jscode/hyperlink.js
+++ anarchistbookdistro/jscode/hyperlink.js
@@ -4,40 +4,12 @@ hyperlink = {
     "alt": "cardboard sign with trash magic logo",
     "list": [
         {
-            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
-            "text": "BUILD TRASH MAGIC MEDIA SERVER"
-        },
-        {
-            "href": "scroll.html",
-            "text": "TRASH MAGIC SCROLL"
-        },
-        {
-            "href": "hyperlink/",
-            "text": "HYPERLINK PAGE BUILDER"
-        },
-        {
-            "href": "web/",
-            "text": "WEB"
-        },
-        {
-            "href": "images.html",
-            "text": "IMAGES"
+            "href": "http://10.0.0.138/",
+            "text": "http://10.0.0.138/"
         },
         {
             "href": "media.html",
-            "text": "MEDIA"
-        },
-        {
-            "href": "fork.html",
-            "text": "FORK"
-        },
-        {
-            "href": "https://www.trashrobot.org",
-            "text": "www.trashrobot.org"
-        },
-        {
-            "href": "https://www.southplatte.net",
-            "text": "southplatte.net"
+            "text": "BOOK DEPOSITORY"
         }
     ]
 };
