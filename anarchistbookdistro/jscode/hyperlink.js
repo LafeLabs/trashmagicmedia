1e0eca57e3e49ffcbf50b96a7432c56411ebe449
Trash Robot <lafelabs@gmail.com>
2022-08-21 15:08:59 -0400

    anarchist book distro

--- anarchistbookdistro/jscode/hyperlink.js
+++ anarchistbookdistro/jscode/hyperlink.js
@@ -1,15 +1,23 @@
 hyperlink = {
     "url": "http://10.0.0.27/",
-    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/anarchistbookdistro/images/replicator.svg",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/anarchistbookdistro/images/replicator.png",
     "alt": "cardboard sign with trash magic logo",
     "list": [
         {
-            "href": "http://10.0.0.138/",
-            "text": "http://10.0.0.138/"
+            "href": "https://www.eventbrite.com/",
+            "text": "EVENTBRITE CLASS"
+        },
+        {
+            "href": "hyperlink/",
+            "text": "HYPERLINK/"
         },
         {
             "href": "media.html",
             "text": "BOOK DEPOSITORY"
+        },
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/anarchistbookdistro/README.md",
+            "text": "Replicate anarchist book distro server"
         }
     ]
 };
