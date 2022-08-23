e45cc8b3eaf655e08c2660d239563a776ab46dbc
Trash Robot <lafelabs@gmail.com>
2022-08-23 17:51:25 -0400

    blart replicator

--- blart/jscode/hyperlink.js
+++ blart/jscode/hyperlink.js
@@ -1,11 +1,11 @@
 hyperlink = {
-    "url": "http://10.0.0.27/",
-    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/trashmagic.jpg",
+    "url": "http://10.0.0.7/",
+    "src": "https://f4.bcbits.com/img/a1743870227_16.jpg",
     "alt": "cardboard sign with trash magic logo",
     "list": [
         {
-            "href": "scroll.html",
-            "text": "SCROLLS"
+            "href": "media.html",
+            "text": "MEDIA"
         },
         {
             "href": "media.html",
@@ -28,48 +28,8 @@ hyperlink = {
             "text": "buy blart on bandcamp for $4.20"
         },
         {
-            "href": "ThIIIrd Person - Blart(in Looper Kimble III) - 01 BLART.mp3",
-            "text": "blart.mp3"
-        },
-        {
-            "href": "editor.php",
-            "text": "editor.php"
-        },
-        {
-            "href": "https://github.com/LafeLabs/trashmagicmedia",
-            "text": "BUILD TRASH MAGIC MEDIA SERVER"
-        },
-        {
-            "href": "scroll.html",
-            "text": "TRASH MAGIC SCROLLS"
-        },
-        {
-            "href": "hyperlink/",
-            "text": "HYPERLINK PAGE BUILDER"
-        },
-        {
             "href": "web/",
             "text": "WEB"
-        },
-        {
-            "href": "images.html",
-            "text": "IMAGES"
-        },
-        {
-            "href": "media.html",
-            "text": "MEDIA"
-        },
-        {
-            "href": "fork.html",
-            "text": "FORK"
-        },
-        {
-            "href": "https://www.trashrobot.org",
-            "text": "www.trashrobot.org"
-        },
-        {
-            "href": "https://www.southplatte.net",
-            "text": "southplatte.net"
         }
     ]
 };
