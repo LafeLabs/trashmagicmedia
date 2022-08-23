ebd0be0840ebfcc2be5c89a45b2256e8b2d6fb4f
Trash Robot <lafelabs@gmail.com>
2022-08-23 17:02:29 -0400

    blart song replicator

---
+++ blart/README.md
@@ -0,0 +1,12 @@
+# Trash Magic Media
+
+# Blart self-replicating fan web page
+
+
+# Replicator
+
+1. [Learn what the Innernette is by clicking this youtube.com link](https://www.youtube.com/embed/Y5BZkaWZAAA)
+1. [download XAMPP](https://www.apachefriends.org/index.html) to a windows or mac laptop and install
+1. Start the server
+1. Navigate to the folder c:\xampp\htdocs\ and delete the file index.php
+1. Copy the file [replicator.php]() to 
---
+++ blart/jscode/hyperlink.js
@@ -0,0 +1,75 @@
+hyperlink = {
+    "url": "http://10.0.0.27/",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/images/trashmagic.jpg",
+    "alt": "cardboard sign with trash magic logo",
+    "list": [
+        {
+            "href": "scroll.html",
+            "text": "SCROLLS"
+        },
+        {
+            "href": "media.html",
+            "text": "MEDIA"
+        },
+        {
+            "href": "images.html",
+            "text": "IMAGES"
+        },
+        {
+            "href": "https://www.youtube.com/embed/Y5BZkaWZAAA",
+            "text": "Learn About the Innernette"
+        },
+        {
+            "href": "web/",
+            "text": "Go onto the Innernette"
+        },
+        {
+            "href": "https://thiiirdperson.bandcamp.com/album/blart-in-looper-kimble-iii",
+            "text": "buy blart on bandcamp for $4.20"
+        },
+        {
+            "href": "ThIIIrd Person - Blart(in Looper Kimble III) - 01 BLART.mp3",
+            "text": "blart.mp3"
+        },
+        {
+            "href": "editor.php",
+            "text": "editor.php"
+        },
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
+        },
+        {
+            "href": "scroll.html",
+            "text": "TRASH MAGIC SCROLLS"
+        },
+        {
+            "href": "hyperlink/",
+            "text": "HYPERLINK PAGE BUILDER"
+        },
+        {
+            "href": "web/",
+            "text": "WEB"
+        },
+        {
+            "href": "images.html",
+            "text": "IMAGES"
+        },
+        {
+            "href": "media.html",
+            "text": "MEDIA"
+        },
+        {
+            "href": "fork.html",
+            "text": "FORK"
+        },
+        {
+            "href": "https://www.trashrobot.org",
+            "text": "www.trashrobot.org"
+        },
+        {
+            "href": "https://www.southplatte.net",
+            "text": "southplatte.net"
+        }
+    ]
+};
