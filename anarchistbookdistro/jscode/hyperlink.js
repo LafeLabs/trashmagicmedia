adefb7395cf0af57cf9f25033083b961de56afa3
Trash Robot <lafelabs@gmail.com>
2022-08-24 18:03:47 -0400

    recursion

--- anarchistbookdistro/jscode/hyperlink.js
+++ anarchistbookdistro/jscode/hyperlink.js
@@ -1,27 +1,31 @@
 hyperlink = {
-    "url": "http://10.0.0.27/",
-    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/anarchistbookdistro/images/replicator.png",
+    "url": "http://10.0.0.7/",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/anarchistbookdistro/images/cerealback.png",
     "alt": "cardboard sign with trash magic logo",
     "list": [
         {
-            "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/README.md",
-            "text": "TRASH MAGIC MEDIA REPLICATOR"
+            "href": "media.html",
+            "text": "MEDIA"
+        },
+        {
+            "href": "images.html",
+            "text": "IMAGES"
+        },
+        {
+            "href": "https://www.youtube.com/channel/UCLyeOlfnEBCnRTAH8rppEFw",
+            "text": "youtube:@trash_robot"
+        },
+        {
+            "href": "https://github.com/LafeLabs/",
+            "text": "github:@lafelabs"
+        },
+        {
+            "href": "https://www.tiktok.com/@trash_robot",
+            "text": "tiktok:@trash_robot"
         },
         {
             "href": "https://github.com/LafeLabs/trashmagicmedia/blob/main/anarchistbookdistro/README.md",
             "text": "ANARCHIST BOOK DISTRO REPLICATOR"
-        },
-        {
-            "href": "https://www.eventbrite.com/e/anarchist-book-distro-on-the-trash-magic-network-tickets-405907990727",
-            "text": "EVENTBRITE CLASS"
-        },
-        {
-            "href": "hyperlink/",
-            "text": "HYPERLINK/"
-        },
-        {
-            "href": "media.html",
-            "text": "BOOK DEPOSITORY"
         }
     ]
 };
