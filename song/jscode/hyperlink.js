0ad670ca0977e2952f8117f4a148fc1c025dd73e
Trash Robot <lafelabs@gmail.com>
2022-09-01 14:45:22 -0400

    song replicator copied from blart, but as seed for generic

---
+++ song/jscode/hyperlink.js
@@ -0,0 +1,39 @@
+hyperlink = {
+    "url": "https://github.com/LafeLabs/trashmagicmedia/tree/main/song",
+    "src": "https://f4.bcbits.com/img/a1743870227_16.jpg",
+    "alt": "cardboard sign with trash magic logo",
+    "list": [
+        {
+            "href": "scroll.html",
+            "text": "SCROLLS"
+        },
+        {
+            "href": "https://thiiirdperson.bandcamp.com/album/blart-in-looper-kimble-iii",
+            "text": "Buy blart on bandcamp for $4.20"
+        },
+        {
+            "href": "http://tiktok.com/@thiiirdperson",
+            "text": "Thiiirdperson on Tiktok"
+        },
+        {
+            "href": "https://www.patreon.com/thiiirdperson/",
+            "text": "Thiiirdperson on Patreon"
+        },
+        {
+            "href": "https://thiiirdperson.bandcamp.com/",
+            "text": "Thiiirdperson on Bandcamp"
+        },
+        {
+            "href": "http://twitter.com/thiiirdperson",
+            "text": "Thiiirdperson on Twitter"
+        },
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/tree/main/song",
+            "text": "REPLICATOR"
+        },
+        {
+            "href": "media.html",
+            "text": "MEDIA"
+        }
+    ]
+};
