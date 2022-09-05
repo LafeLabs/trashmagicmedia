c6c8b83cdf2751d11cb1cd73ffcbc3e9c6f533a8
Trash Robot <lafelabs@gmail.com>
2022-09-05 16:18:24 -0400

    edgephysicsdotorg

---
+++ hyperlink/edgephysicsdotorg/README.md
@@ -0,0 +1,9 @@
+# [Hyperlink](https://github.com/LafeLabs/hyperlink)
+
+## By [TRASH ROBOT](https://www.trashrobot.org)
+
+## [http://localhost/](http://localhost/)
+
+## [global replicator link](https://raw.githubusercontent.com/LafeLabs/hyperlink/main/php/replicator.txt)
+
+
---
+++ hyperlink/edgephysicsdotorg/jscode/hyperlink.js
@@ -0,0 +1,51 @@
+hyperlink = {
+    "url": "https://www.trashrobot.org",
+    "src": "https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/scripture/images/replicator.png",
+    "alt": "cardboard sign which says trash magic and trash can with eight arrows of chaos",
+    "list": [
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia/tree/main/anarchistbookdistro",
+            "text": "TRASH MAGIC MEDIA NETWORK ANARCHIST BOOK DISTRO"
+        },
+        {
+            "href": "https://github.com/LafeLabs/trashmagicmedia",
+            "text": "BUILD TRASH MAGIC MEDIA SERVER"
+        },
+        {
+            "href": "https://www.lulu.com/shop/trash-robot/geometron-magic/paperback/product-y6p96w.html",
+            "text": "buy geometron magic book from Lulu press"
+        },
+        {
+            "href": "https://www.trashrobot.org/user.php?scroll=scrolls/trashmagicbooks",
+            "text": "other trash magic books"
+        },
+        {
+            "href": "https://www.trashrobot.org/geometronmagic/",
+            "text": "geometron magic read online"
+        },
+        {
+            "href": "https://www.trashrobot.org/bookofgeometronfirst/",
+            "text": "first book of geometron read online"
+        },
+        {
+            "href": "https://www.tiktok.com/@trash_robot",
+            "text": "tiktok:@trash_robot"
+        },
+        {
+            "href": "https://www.instagram.com/lafelabs/",
+            "text": "instagram:@lafelabs"
+        },
+        {
+            "href": "https://www.youtube.com/channel/UCLyeOlfnEBCnRTAH8rppEFw",
+            "text": "youtube:@trash_robot"
+        },
+        {
+            "href": "https://github.com/LafeLabs/",
+            "text": "github:@lafelabs"
+        },
+        {
+            "href": "copy.php?from=https://raw.githubusercontent.com/LafeLabs/trashmagicmedia/main/hyperlink/edgephysicsdotorg/jscode/hyperlink.js&to=jscode/hyperlink.js",
+            "text": "UPDATE THIS HYPERLINK PAGE"
+        }
+    ]
+};
