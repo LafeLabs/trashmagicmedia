821396772839ee4fada1705ee1550f98955423ac
Trash Robot <lafelabs@gmail.com>
2022-10-09 17:08:28 -0600

    data server work

--- sensorserver/measure.py
+++ sensorserver/measure.py
@@ -2,18 +2,22 @@
 import serial
 import time
 import datetime
-presentDate = datetime.datetime.now()
-unix_timestamp = datetime.datetime.timestamp(presentDate)*1000
-ser = serial.Serial('COM3', 9600, timeout=1)
-ser.flush()
-i = 0
-data = ""
-while i < 10:
-  i += 1
-  data += str(ser.readline())
-ser.close()
-data = data.split("\\r\\n\'b\'")[5]
-filename = "media/sensordata/data" + str(unix_timestamp).split(".")[0] + ".txt"
-file1 = open(filename,'w')
-file1.write(data)
-file1.close()    
+
+
+while True:
+    presentDate = datetime.datetime.now()
+    unix_timestamp = datetime.datetime.timestamp(presentDate)*1000
+    ser = serial.Serial('COM3', 9600, timeout=1)
+    ser.flush()
+    i = 0
+    data = ""
+    while i < 10:
+      i += 1
+      data += str(ser.readline())
+    ser.close()
+    data = data.split("\\r\\n\'b\'")[5]
+    filename = "media/sensordata/data" + str(unix_timestamp).split(".")[0] + ".txt"
+    file1 = open(filename,'w')
+    file1.write(data)
+    file1.close()    
+    sleep(60)# wait 60 seconds, so record a data point every minute until program break
